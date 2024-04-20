35ff247c1c930936323fe1f38d810acd1acce46c
Redines <pontus.goth@outlook.com>
2024-04-20 15:14:07 +0200

    adding the first basic structures of clean architecture. added .gitignore added example classes for entity, handlers etc.

---
+++ Application/Features/Examples/ExampleListVm.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.Examples
+{
+    //Vm = ViewModel, describing what we want to retreive
+    public class ExampleListVm
+    {
+        public Guid ExampleId { get; set; }
+        public string ExampleName { get; set; }
+    }
+}
---
+++ Application/Features/Examples/GetExamplesListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Application.Contracts.Persistance;
+using AutoMapper;
+using MediatR;
+
+namespace Application.Features.Examples
+{
+    //Handler will contain the actual behaviour and execute through a request and return the Vm with wanted data
+    public class GetExamplesListQueryHandler : IRequestHandler<GetExamplesListQuery,List<ExampleListVm>>
+    {
+        private readonly IAsyncRepository<ExampleListVm> _exampleListVmRepository;
+        private readonly IMapper _mapper;
+
+        public GetExamplesListQueryHandler(IAsyncRepository<ExampleListVm> exampleListVmRepository, IMapper mapper)
+        {
+            _exampleListVmRepository = exampleListVmRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ExampleListVm>> Handle(GetExamplesListQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = (await _exampleListVmRepository.ListAllAsync()).OrderBy(x => x.ExampleName);
+            return _mapper.Map<List<ExampleListVm>>(allEvents);
+        }
+    }
+}
